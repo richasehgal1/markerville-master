2c37dd0f6d6e4d38b6bc399815432c5de4f430fe
richasehgal1 <30476764+richasehgal1@users.noreply.github.com>
2017-08-01 12:05:41 -0700

    Add files via upload

--- public/js/fetchData.js
+++ public/js/fetchData.js
@@ -1,9 +1,9 @@
 //make sure is called
 async function onSearchData(event) {
   event.preventDefault();
-  const input = document.querySelectorAll("[id^='#word-input']");
+  const input = document.querySelector('#word-input');
   const word = input.value.trim();
-  const results = document.querySelectorAll("[id^='#database']");
+  const results = document.querySelector('#database');
   const result = await fetch('/lookup/' + word);
   const json = await result.json();
   const resultWord = json.word;
@@ -34,5 +34,5 @@ async function onSearchData(event) {
 
 
 var table = $('#result table').DataTable();
-const searchForm = document.querySelectorAll("[id^='#searchForm']");
+const searchForm = document.querySelector('#searchForm');
 searchForm.addEventListener('submit', onSearchData);
